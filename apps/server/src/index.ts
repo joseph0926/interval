97782bebd36db30c82135dd096b198404dca93a8
joseph0926 <joseph0926@naver.com>
2025-11-30 08:12:42 +0900

    feat: shutdown 로직 추가

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,6 +7,15 @@ async function main() {
 	const { HOST, PORT } = app.getEnvs<Env>();
 
 	await app.listen({ port: PORT, host: HOST });
+
+	const shutdown = async (signal: string) => {
+		app.log.info(`${signal} received, shutting down...`);
+		await app.close();
+		process.exit(0);
+	};
+
+	process.on("SIGINT", () => shutdown("SIGINT"));
+	process.on("SIGTERM", () => shutdown("SIGTERM"));
 }
 
 main().catch((err) => {
